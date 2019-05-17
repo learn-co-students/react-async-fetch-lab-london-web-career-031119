459cb8c336243e4b6226979db359516a461b6bf5
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-17 09:23:28 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
-// create your App component here
+import React, { Component } from "react";
+class App extends Component {
+  state = {
+    number: null,
+    people: []
+  };
+
+  displayData = object => {
+    this.setState({ number: object["number"], people: object["people"] });
+  };
+
+  componentDidMount() {
+    return fetch("http://api.open-notify.org/astros.json")
+      .then(resp => resp.json())
+      .then(data => this.displayData(data));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.number} people in space</p>
+      </div>
+    );
+  }
+}
+
+export default App;
