1c5884c395da89179cb499149ce116b902a2100e
oli-gra <oliver.grant@gmail.com>
2019-05-19 21:14:04 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,37 @@
 // create your App component here
+import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react'
+
+const URL = 'http://api.open-notify.org/astros.json'
+
+class App extends Component {
+   state = {
+      PeopleInSpace: []
+   }
+
+   getStuff() {
+      fetch(URL)
+         .then(r => r.json())
+         .then(data => this.setState({ PeopleInSpace: data.people }))
+         .catch(error => alert(error))
+   }
+
+   componentDidMount() {
+      this.getStuff()
+   }
+
+   render() {
+      return (
+         <React.Fragment>
+            {this.state.PeopleInSpace.map(person =>
+               <Card>
+                  <Card.Content header={person.name} />
+                  <Card.Content description={person.craft} />
+               </Card>
+            )}
+         </React.Fragment>
+      );
+   }
+}
+
+export default App;
